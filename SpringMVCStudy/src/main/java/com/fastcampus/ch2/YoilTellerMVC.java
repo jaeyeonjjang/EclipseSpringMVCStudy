8acd242d898f297aca027b626acadd12942d423b
jaeyeonjjang <wodus0936@gmail.com>
2023-05-29 23:48:27 +0900

    서블릿과 컨트롤러의 비교

--- SpringMVCStudy/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ SpringMVCStudy/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,60 +1,46 @@
 package com.fastcampus.ch2;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-// 년월일을 입력하면 요일을 알려주는 프로그램.
 public class YoilTellerMVC {
-    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1{
-//    public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	    public ModelAndView main(int year, int month, int day) throws IOException {
-	      // ModelAndView는 반환타입으로 Model을 만들지 않고 여기에 모델을 만든느거임. 근데 잘 안씀 
-	    	// Model을 매개변수로 선언하면 DispatcherServlet이 모델 생성함.
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
+ 
+        // 1. 유효성 검사
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
     	
-    		ModelAndView mv = new ModelAndView(); //Model 생성 
-    		// 1. 유효성 검사
-//	    	if(!isValid(year, month, day))
-//	    		return "yoilError";
-//	
-	    	// 2. 요일 계산
-	        char yoil = getYoil(year, month, day);
-	
-	        // 3. 계산한 결과를 model에 저장
-	        mv.addObject("year",year);
-	        mv.addObject("month",month);
-	        mv.addObject("day",day);
-	        mv.addObject("yoil",yoil);
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
+
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
         
-//	        4. 결과를 보여줄 view의 이름을 지정
-	        mv.setViewName("yoil");
-	        
-	        // 5. ModelAndView를 반환 
-	        return mv;
-//        return "yoil"; ///WEB_INF/views/yoil.jsp
-
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
     }
-
-    //class 안에서만 쓰게하기 위해서 private로 
-	private boolean isValid(int year, int month, int day) {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	private char getYoil(int year, int month, int day) {
-		Calendar cal = Calendar.getInstance();
+    
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
         cal.set(year, month - 1, day);
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        return " 일월화수목금토".charAt(dayOfWeek);   // 일요일:1, 월요일:2, ... 
-	}
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+    }
 }
