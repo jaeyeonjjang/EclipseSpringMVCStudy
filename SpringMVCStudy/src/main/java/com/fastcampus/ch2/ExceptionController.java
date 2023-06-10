c55225131852bb09a660597587acaecbb99005c1
jaeyeonjjang <wodus0936@gmail.com>
2023-06-10 15:18:29 +0900

    예외처리2

--- SpringMVCStudy/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ SpringMVCStudy/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -2,10 +2,12 @@ package com.fastcampus.ch2;
 
 import java.io.FileNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
@@ -18,10 +20,11 @@ public class ExceptionController {
 	}
 	
 	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //상태코드를 500으로 바꿈.
 	public String catcher(Exception ex, Model m) { //캐치블럭 메서드임. 모든 exception을 얘가 받아냄.
 		System.out.println("catcher() in ExceptionController");
 		System.out.println("m="+m);
-		m.addAttribute("ex", ex);
+//		m.addAttribute("ex", ex);
 		return "error";
 	}
 	
--- SpringMVCStudy/src/main/java/com/fastcampus/ch2/GloberCatcher.java
+++ SpringMVCStudy/src/main/java/com/fastcampus/ch2/GloberCatcher.java
@@ -6,19 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice("com.fastcampus.ch2") // 패키지를 지정해줄수 있음.
-//@ControllerAdvice // 모든 패키지에 적용됨. 컨트롤러에서 발생하는 예외들을 처리해줌.
-public class GloberCatcher {
-	@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
-	public String catcher2(Exception ex, Model m) {
-		m.addAttribute("ex", ex);
-		return "error";
-	}
-	
-	@ExceptionHandler(Exception.class)
-	public String catcher(Exception ex, Model m) { //캐치블럭 메서드임. 모든 exception을 얘가 받아냄.
-		m.addAttribute("ex", ex);
-		return "error";
-	}
+//	@ControllerAdvice("com.fastcampus.ch2") // 패키지를 지정해줄수 있음.
+//	//@ControllerAdvice // 모든 패키지에 적용됨. 컨트롤러에서 발생하는 예외들을 처리해줌.
+//	public class GloberCatcher {
+//		@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
+//		public String catcher2(Exception ex, Model m) {
+//			m.addAttribute("ex", ex);
+//			return "error";
+//		}
+//	
+//	@ExceptionHandler(Exception.class)
+//	public String catcher(Exception ex, Model m) { //캐치블럭 메서드임. 모든 exception을 얘가 받아냄.
+//		m.addAttribute("ex", ex);
+//		return "error";
+//	}
 
-}
+//}
